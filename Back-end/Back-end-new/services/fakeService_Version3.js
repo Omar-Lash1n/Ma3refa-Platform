cf8bbe43b05ea61ee60fc13440979a65ce0dce30
ahmedabosayed777 <ahmedabosayed258@gmail.com>
2026-01-25 02:52:21 +0200

    Delete Back-end directory

--- Back-end/Back-end-new/services/fakeService_Version3.js
+++
@@ -1,5 +0,0 @@
-function createFakePaymentLink({ paymentId, baseUrl }) {
-  return `${baseUrl}/api/payments/webhook/simulate?paymentId=${paymentId}`;
-}
-
-module.exports = { createFakePaymentLink };
